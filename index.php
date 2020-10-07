4b9d40e33566de101919550487bf4a833fab8d83
khawarhere <khawarhere@gmail.com>
2020-10-07 16:37:08 +0500

    change ver 1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Stack</title>
+</head>
+
+<body>
+
+<section class="container">
+    <div class="w100 main-heading">
+        <h1>Let's Get Started with Best
+            PHP Cloud Hosting</h1>
+            <h2> Here is change version 01 </h2>
+    </div>
+</section>
+
+<footer><div class="container text-center">&copy; 2011-<?=date('Y');?> © Cloudways Ltd. All right reserved</div></footer>
+</body>
+</html>
